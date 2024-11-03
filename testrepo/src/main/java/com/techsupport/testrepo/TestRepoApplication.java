91b7c0889827cd2d53b4658d89101d1174169238
Zohair-Khan <zohairkhanmail@gmail.com>
2024-11-02 20:49:33 -0500

    zohair adding a comment to application controller

--- testrepo/src/main/java/com/techsupport/testrepo/TestRepoApplication.java
+++ testrepo/src/main/java/com/techsupport/testrepo/TestRepoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestRepoApplication {
-
+	//Zohair's comment
 	public static void main(String[] args) {
 		SpringApplication.run(TestRepoApplication.class, args);
 	}
